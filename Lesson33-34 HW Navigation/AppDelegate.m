5f896965d71c9c92a39e8fb70ac06aa04662a688
xhardcorex <nik@MacBook-Air-Nik.local>
2015-08-19 15:34:35 +0300

    start

--- Lesson33-34 HW Navigation/AppDelegate.m
+++ Lesson33-34 HW Navigation/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "NSTableViewController.h"
 
 @interface AppDelegate ()
 
---
+++ NSTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NSTableViewController.h
+//  Lesson33-34 HW Navigation
+//
+//  Created by Nik on 19.08.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NSTableViewController : UITableViewController
+
+@end
