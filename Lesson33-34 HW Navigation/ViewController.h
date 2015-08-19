c2189d9a256c1ed54bc43f74cbce573dd0f1d5bb
xhardcorex <nik@MacBook-Air-Nik.local>
2015-08-19 15:16:52 +0300

    Initial Commit

---
+++ Lesson33-34 HW Navigation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Lesson33-34 HW Navigation
+//
+//  Created by Nik on 19.08.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Lesson33-34 HW Navigation/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Lesson33-34 HW Navigation
+//
+//  Created by Nik on 19.08.15.
+//  Copyright (c) 2015 Nik. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
